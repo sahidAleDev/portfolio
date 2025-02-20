92bc668abf4a7efcad7f735aa03ecce992a0e753
sahidAle <sahidaledev@gmail.com>
2025-02-20 13:25:07 -0600

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
+	screens: {
+		'sm': '576px',
+		// => @media (min-width: 576px) { ... }
+
+		'md': '960px',
+		// => @media (min-width: 960px) { ... }
+
+		'lg': '1440px',
+		// => @media (min-width: 1440px) { ... }
+	},
+	plugins: [],
+}
